323e585cfd12783bf83790c763db4ee397aac261
rosskovt <rostikk@gmail.com>
2023-01-03 18:04:25 +0200

    album component added and updates

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,6 +2,8 @@ import Button from "./Button";
 import { GoTrashcan } from "react-icons/go";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/use-thunk";
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumsList from "./AlbumsList";
 
 function UsersListItem({ user }) {
     const [doRemoveUser, isLoading, error] = useThunk(removeUser);
@@ -10,20 +12,20 @@ function UsersListItem({ user }) {
         doRemoveUser(user);
     };
 
+    const header = <>
+        <Button className="mr-3" onClick={() => handleUserDelete(user)} loading={isLoading}>
+            <GoTrashcan />
+        </Button>
+        {
+            error && <div>"Error deletinng user"</div>
+        }
+        {user.name}
+    </>;
+
     return (
-        <div key={user.id} className="mb-2 border rounded">
-            <div className='flex p-2 justify-between items-center cursor-pointer'>
-                <div className="flex flex-row justify-between items-center">
-                    <Button className="mr-3" onClick={() => handleUserDelete(user)} loading={isLoading}>
-                        <GoTrashcan />
-                    </Button>
-                    {
-                        error && <div>"Error deletinng user"</div>
-                    }
-                    {user.name}
-                </div>
-            </div>
-        </div>
+        <ExpandablePanel header={header}>
+            <AlbumsList user={user} />
+        </ExpandablePanel>
     )
 }
 
